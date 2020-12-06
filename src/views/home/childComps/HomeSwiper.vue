27efc3eac93f4e1da4edde9d87117c96db131937
renzihao206 <2213405093@qq.com>
2020-12-07 00:46:32 +0800

    HYMall

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <swiper ref="swiper" v-if="banners.length">
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'common/swiper'
+
+	export default {
+		name: "HomeSwiper",
+    components: {
+		  Swiper,
+      SwiperItem
+    },
+    props: {
+		  banners: {
+		    type: Array,
+        default: []
+      }
+    },
+    data(){
+		  return {
+		    isload:false
+      }
+    },
+    methods: {
+		  stopTimer() {
+		    this.$refs.swiper.stopTimer()
+      },
+      startTimer() {
+		    if (this.$refs.swiper) {
+          this.$refs.swiper.startTimer()
+        }
+      },
+      imageLoad(){
+if(!this.isload){
+  this.$emit('swiperImageLoad')
+  this.isload = true
+}
+
+      }
+
+    }
+	}
+</script>
+
+<style scoped>
+
+</style>
